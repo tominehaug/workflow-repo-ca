11d2a011540ac97bddcebed97c31c6d9312860c2
tominehaug <tomine_haug@live.no>
2026-05-14 11:41:08 +0200

    deleted comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,13 +10,13 @@ export default defineConfig([
     languageOptions: {
       globals: {
         ...globals.browser,
-        describe: true, // Used for grouping tests
-        test: true, // Used to create tests
-        it: true, // Alternative way to create tests
-        expect: true, // Used for test assertions
-        require: true, // Used in Node.js files like Tailwind config
-        module: true, // Used in Node.js files like Tailwind config
-        process: true, // Used for environment variables later
+        describe: true,
+        test: true,
+        it: true,
+        expect: true,
+        require: true,
+        module: true,
+        process: true,
       },
     },
   },
