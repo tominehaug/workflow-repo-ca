13ccb567941662d9f1f73c7dc2d3d61cfa74b794
tominehaug <tomine_haug@live.no>
2026-05-21 11:57:27 +0200

    formatted with prettier

--- js/utils/userInterface.test.js
+++ js/utils/userInterface.test.js
@@ -4,37 +4,37 @@ import { expect, describe, it } from "vitest";
 // test
 
 describe("isActivePath", () => {
-    it("returns true when current path matches href exactly", () => {
-        const href = "/about";
-        const currentPath ="/about";
-        const result = isActivePath(href, currentPath);
-        expect(result).toBe(true);
-    });
+  it("returns true when current path matches href exactly", () => {
+    const href = "/about";
+    const currentPath = "/about";
+    const result = isActivePath(href, currentPath);
+    expect(result).toBe(true);
+  });
 
-    it(`returns true for root path ("/") when path is "/"`, () => {
-        const href = "/";
-        const currentPath = "/";
-        const result = isActivePath(href, currentPath);
-        expect(result).toBe(true);
-    });
+  it(`returns true for root path ("/") when path is "/"`, () => {
+    const href = "/";
+    const currentPath = "/";
+    const result = isActivePath(href, currentPath);
+    expect(result).toBe(true);
+  });
 
-    it(`returns true for root path ("/") when path is "/index.html"`, () => {
-        const href = "/";
-        const currentPath = "/index.html";
-        const result = isActivePath(href, currentPath);
-        expect(result).toBe(true);
-    });
+  it(`returns true for root path ("/") when path is "/index.html"`, () => {
+    const href = "/";
+    const currentPath = "/index.html";
+    const result = isActivePath(href, currentPath);
+    expect(result).toBe(true);
+  });
 
-    it("returns true when current path includes the href", () => {
-        const href = "/about";
-        const currentPath = "/about/contact.html"
-        const result = isActivePath(href, currentPath);
-        expect(result).toBe(true);
-    });
-        it("returns false when paths don't match", () => {
-        const href = "/about";
-        const currentPath = "/venue";
-        const result = isActivePath(href, currentPath);
-        expect(result).toBe(false);
-    });
+  it("returns true when current path includes the href", () => {
+    const href = "/about";
+    const currentPath = "/about/contact.html";
+    const result = isActivePath(href, currentPath);
+    expect(result).toBe(true);
+  });
+  it("returns false when paths don't match", () => {
+    const href = "/about";
+    const currentPath = "/venue";
+    const result = isActivePath(href, currentPath);
+    expect(result).toBe(false);
+  });
 });
